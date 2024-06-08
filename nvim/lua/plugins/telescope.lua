d285b0f34afa56eaa57c0a7dc7766e3040fb98af
Gabino Hernandez <gabino.hern.g@gmail.com>
2024-06-08 08:16:32 -0600

    agregaddo de configuracion personal de neovim

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,15 @@
+return {
+  'nvim-telescope/telescope.nvim', tag = '0.1.5',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    local builtin = require("telescope.builtin")
+    --vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+    vim.keymap.set('n', '<C-p>', function() builtin.find_files({ cwd = vim.fn.expand("%:p:h"), hidden = true }) end, {})
+
+    --
+    vim.keymap.set('n', '<C-j>', builtin.live_grep, {})
+    --vim.keymap.set('n', '<C-k>', function() builtin.live_grep({ search = vim.fn.expand("<cword>") }) end, {})
+    vim.keymap.set('n', '<C-k>', function() builtin.live_grep({search_dirs={vim.fn.expand("%:p")}}) end, {})
+  end
+}
+
