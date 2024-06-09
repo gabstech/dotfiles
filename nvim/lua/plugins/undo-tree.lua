7346765e7d8062c554dd25e75de5e4dc668313fb
Gabino Hernandez <gabino.hern.g@gmail.com>
2024-06-09 15:47:01 -0600

    Agregado de identacion correcto

--- nvim/lua/plugins/undo-tree.lua
+++ nvim/lua/plugins/undo-tree.lua
@@ -1,8 +1,9 @@
-return {
-  "jiaoshijie/undotree",
-  dependencies = "nvim-lua/plenary.nvim",
-  config = true,
-  keys = { -- load the plugin only when using it's keybinding:
-    { "<C-u>", "<cmd>lua require('undotree').toggle()<cr>" },
-  },
-}
+return 
+  {
+    "jiaoshijie/undotree",
+    dependencies = "nvim-lua/plenary.nvim",
+    config = true,
+    keys = { -- load the plugin only when using it's keybinding:
+      { "<C-u>", "<cmd>lua require('undotree').toggle()<cr>" },
+    },
+  }
